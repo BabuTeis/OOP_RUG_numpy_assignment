87c1333e9b8151c28f103808d500588d4c1ad907
BabuTeis <huizingastijn@gmail.com>
2024-09-15 15:43:24 +0200

    Final implementation of numpy assignment

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# Create a list of numbers from 1 to 10
+numbers = list(range(1, 11))
+
+# Convert the list into a numpy array
+arr = np.array(numbers)
+
+# Print the numpy array
+print(arr)
